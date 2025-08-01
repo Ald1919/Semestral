92b3e5d3dd2792f2f118b82f78369202bd08f4f3
Liyi-He <teresa2056805725@gmail.com>
2025-07-31 19:14:59 -0500

    Update Portero.java

--- Proyecto-Semestral/src/Semestral/Portero.java
+++ Proyecto-Semestral/src/Semestral/Portero.java
@@ -2,79 +2,124 @@ package Semestral;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Portero {
     private int x, y;
-    private final int ancho = 80;
-    private final int alto = 80;
-    private final int velocidad = 10;
-    private int destinoX, destinoY;
-    private boolean animando = false;
-    private final Timer timer;
-    private final JComponent componenteParaRepintar;
-    private final int originalX;
-    private final int originalY;
+    private final int ancho = 60, alto = 90;
+    private Timer timerMovimiento;
+    private final int xInicial, yInicial;
+    private boolean regresandoAlCentro = false;
+    private final JPanel parentPanel;
 
-    public Portero(int x, int y, JComponent componente) {
+    public Portero(int x, int y, JPanel parentPanel) {
         this.x = x;
         this.y = y;
-        this.originalX = x;
-        this.originalY = y;
-        this.componenteParaRepintar = componente;
-
-        timer = new Timer(20, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                moverPaso();
-            }
-        });
+        this.xInicial = x;
+        this.yInicial = y;
+        this.parentPanel = parentPanel;
     }
 
     public void moverA(int nuevoX, int nuevoY) {
-        this.destinoX = nuevoX;
-        this.destinoY = nuevoY;
-        this.animando = true;
-        timer.start();
-    }
+        detenerAnimacion();
+        this.regresandoAlCentro = false;
 
-    private void moverPaso() {
-        if (x == destinoX && y == destinoY) {
-            animando = false;
-            timer.stop();
-            return;
-        }
+        final int inicioX = x;
+        final int inicioY = y;
+        final int totalPasos = 15;
+        int[] pasos = {0};
 
-        if (x < destinoX) x = Math.min(x + velocidad, destinoX);
-        else if (x > destinoX) x = Math.max(x - velocidad, destinoX);
-
-        if (y < destinoY) y = Math.min(y + velocidad, destinoY);
-        else if (y > destinoY) y = Math.max(y - velocidad, destinoY);
-
-        componenteParaRepintar.repaint();
+        timerMovimiento = new Timer(20, e -> {
+            if (pasos[0] >= totalPasos) {
+                detenerAnimacion();
+                x = nuevoX;
+                y = nuevoY;
+                parentPanel.repaint();
+            } else {
+                float ratio = (float)pasos[0]/totalPasos;
+                x = (int)(inicioX + (nuevoX - inicioX) * ratio);
+                y = (int)(inicioY + (nuevoY - inicioY) * ratio);
+                pasos[0]++;
+                parentPanel.repaint();
+            }
+        });
+        timerMovimiento.start();
     }
 
     public void regresarAlCentro() {
-        moverA(originalX, originalY);
+        detenerAnimacion();
+        this.regresandoAlCentro = true;
+
+        final int inicioX = x;
+        final int inicioY = y;
+        final int totalPasos = 15;
+        int[] pasos = {0};
+
+        timerMovimiento = new Timer(20, e -> {
+            if (pasos[0] >= totalPasos) {
+                detenerAnimacion();
+                x = xInicial;
+                y = yInicial;
+                regresandoAlCentro = false;
+                parentPanel.repaint();
+            } else {
+                float ratio = (float)pasos[0]/totalPasos;
+                x = (int)(inicioX + (xInicial - inicioX) * ratio);
+                y = (int)(inicioY + (yInicial - inicioY) * ratio);
+                pasos[0]++;
+                parentPanel.repaint();
+            }
+        });
+        timerMovimiento.start();
     }
 
     public void detenerAnimacion() {
-        timer.stop();
-        animando = false;
+        if (timerMovimiento != null) {
+            timerMovimiento.stop();
+            timerMovimiento = null;
+        }
     }
 
     public boolean estaAnimando() {
-        return animando;
-    }
-
-    public void dibujar(Graphics g) {
-        g.setColor(new Color(255, 165, 0));
-        g.fillRect(x, y, ancho, alto);
+        return timerMovimiento != null && timerMovimiento.isRunning();
     }
 
     public int getX() { return x; }
     public int getY() { return y; }
     public int getAncho() { return ancho; }
     public int getAlto() { return alto; }
+
+    public void dibujar(Graphics g) {
+        // Cuerpo
+        g.setColor(new Color(0, 0, 200));
+        g.fillRect(x, y + alto/4, ancho, alto * 3/4);
+
+        // Cabeza
+        g.setColor(new Color(255, 205, 150));
+        g.fillOval(x + ancho/4, y, ancho/2, alto/3);
+
+        // Ojos
+        g.setColor(Color.BLACK);
+        g.fillOval(x + ancho/4 + 5, y + alto/8, 5, 5);
+        g.fillOval(x + ancho - ancho/4 - 10, y + alto/8, 5, 5);
+
+        // Brazos
+        g.setColor(new Color(0, 0, 200));
+        g.fillRect(x - 15, y + alto/4 + 5, 15, alto/2);
+        g.fillRect(x + ancho, y + alto/4 + 5, 15, alto/2);
+
+        // Guantes
+        g.setColor(Color.RED);
+        g.fillOval(x - 25, y + alto/4, 25, 20);
+        g.fillOval(x + ancho, y + alto/4, 25, 20);
+
+        // Piernas
+        g.setColor(new Color(150, 150, 150));
+        g.fillRect(x + 5, y + alto - 20, ancho/2 - 5, 20);
+        g.fillRect(x + ancho/2, y + alto - 20, ancho/2 - 5, 20);
+
+        // Zapatos
+        g.setColor(Color.BLACK);
+        g.fillRect(x + 5, y + alto - 10, ancho/2 - 5, 10);
+        g.fillRect(x + ancho/2, y + alto - 10, ancho/2 - 5, 10);
+    }
 }
